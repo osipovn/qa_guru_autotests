0c35e0bcaf760e91fb84059d6f8d9eccb2f3dbaf
Nikita <NAOsipov@sberbank.ru>
2022-08-03 16:17:19 +0300

    add test for Selenide page on GitHub

---
+++ src/test/java/selenideFirst/TestSelenideGitHub.java
@@ -0,0 +1,30 @@
+package selenideFirst;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestSelenideGitHub {
+
+    @BeforeAll
+    static void browser(){
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1280x1024";
+        Configuration.browser = "chrome";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void selenideGitTest(){
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5"));
+    }
+}
